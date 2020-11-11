0176d4c3d0c79f10fd9849040b6b57ce440f5d93
Jennifer Bain <jennifer.fleming.bain@gmail.com>
2020-11-10 17:27:58 -0700

    update about

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,11 +5,42 @@ const About = () => (
   <Layout>
     <h1>About</h1>
     <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea dignissimos
-      aut consequuntur aspernatur corrupti ratione, odit similique tenetur
-      accusantium, est nostrum esse minus iure voluptatum nihil cumque
-      blanditiis non? Odit.
+      Hello! Thank you for stopping by. I'm thrilled you've made it. I'm a web
+      developer with a keen eye for detail. I enjoy problem solving and finding
+      ways to make technology more intuitive for users.
     </p>
+
+    <h1>Coding Languages & Frameworks: </h1>
+    <p>JavaScript | HTML | CSS | Node.js | Ruby on Rails | PSQL</p>
+
+    <h1>Intersted in Learning: </h1>
+    <p>Python | AWS | TypeScript</p>
+
+    <h1>Guilty Pleasure </h1>
+    <p>
+      putzing around in React-Spring and otherwise adding animations to anything
+      and everything
+    </p>
+
+    <h1>Tidbits:</h1>
+    <ul>
+      <li>
+        Trying new recipes and improving on old ones is my jam. I've never met a
+        kitchen experiment I didn't enjoy.
+      </li>
+      <li>
+        Yes, I'm a dog person. I like cats, too. Followed closely by gibbons and
+        snails.
+      </li>
+      <li>
+        My partner, James, is the Scully to my Mulder. We enjoy reality TV,
+        getting scared while camping, and browsing bookstores for treasures.
+      </li>
+      <li>
+        We're currently growing kombucha SCOBYs named Christopher and Nova Scoby
+      </li>
+      <li>I'm easily distracted taking pictures of flowers and rocks.</li>
+    </ul>
   </Layout>
 )
 
