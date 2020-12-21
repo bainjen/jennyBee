b2984b2b679cb7c36b291c594a4f0f6743cff2e0
Jennifer Bain <jennifer.fleming.bain@gmail.com>
2020-12-21 13:12:42 -0700

    work on layout of portfolio piece

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -3,15 +3,25 @@ import { graphql } from "gatsby"
 import Layout from "../components/Layout"
 import styled from "styled-components"
 import { device } from "../devices"
+import Img from "gatsby-image"
 
 const Header = styled.h1`
   font-size: 48px;
   margin: 1em 0;
 `
-const ImageContainer = styled.div``
+const PieceContainer = styled.div`
+  display: flex;
+  position: sticky;
+  height: 80%;
+`
+const ImageContainer = styled.div`
+  width: 50vw;
+`
 const TextContainer = styled.div`
   display: flex;
   flex-direction: column;
+  width: 40vw;
+  margin-right: 5vw;
 `
 const Title = styled.h1`
   font-size: 32px;
@@ -26,31 +36,37 @@ const Description = styled.p`
 `
 
 const RepoLink = styled.a``
-const Portfolio = () => (
-  <Layout>
-    <Header>Selected Projects </Header>
 
-    <TextContainer>
-      <Title>Combine Wizard</Title>
-      <Stack>React | GraphQL | DynamoDB | AWS Amplify | AWS Lambda</Stack>
-      <Description>
-        This project is designed to help farmers evaluate autonomous combine
-        [harvesting tractor] performance. Users can specify auger length, fuel
-        type, and a wheel size to create a combine. Once a configuration is
-        added, a simulation test fires every hour to measure the time that each
-        particular combine takes to plane a 10 x 10 acre field, the percentage
-        of the field covered, and the cost of the run. The efficiency value of
-        each simulation run is averaged to make an overall efficiency measure
-        for each combine configuration.
-      </Description>
-      <RepoLink>visit repository </RepoLink>
-    </TextContainer>
-    <ImageContainer>
-      <img></img>
-    </ImageContainer>
-  </Layout>
-)
-
+const Portfolio = ({ data }) => {
+  console.log("data", data)
+  const tempImg = data.allFile.nodes[0].childImageSharp.fluid
+  return (
+    <Layout>
+      <Header>Selected Projects </Header>
+      <PieceContainer>
+        <TextContainer>
+          <Title>Combine Wizard</Title>
+          <Stack>React | GraphQL | DynamoDB | AWS Amplify | AWS Lambda</Stack>
+          <Description>
+            This project is designed to help farmers evaluate autonomous combine
+            [harvesting tractor] performance. Users can specify auger length,
+            fuel type, and a wheel size to create a combine. Once a
+            configuration is added, a simulation test fires every hour to
+            measure the time that each particular combine takes to plane a 10 x
+            10 acre field, the percentage of the field covered, and the cost of
+            the run. The efficiency value of each simulation run is averaged to
+            make an overall efficiency measure for each combine configuration.
+          </Description>
+          <RepoLink>visit repository </RepoLink>
+        </TextContainer>
+        <ImageContainer>
+          <Img fluid={tempImg} />
+          <Img fluid={tempImg} />
+        </ImageContainer>
+      </PieceContainer>
+    </Layout>
+  )
+}
 export const PortfolioImageQuery = graphql`
   query PortfolioImageQuery {
     allFile(filter: { relativeDirectory: { eq: "portfolioImages" } }) {
